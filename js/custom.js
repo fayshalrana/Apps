12ad09f536ee8b6d89370f36ceadf29a30def4be
Fayshal Rana <114581389+fayshalrana@users.noreply.github.com>
2023-06-03 04:21:23 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// MixitUp JS
+
+var containerEl = document.querySelector('.mixitup_part');
+
+var mixer = mixitup(containerEl);
+
+
+// MixitUp
+
+
+// CounterUP JS
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 3000,
+});
+
+// CounterUP JS
+  
